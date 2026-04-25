7a2f32e6e86b547e0c454779f0730c944e06e118
Snowplou <Snowplou01@gmail.com>
2026-04-24 22:46:26 -0700

    made max memory checker and set more memory permission

--- LA Hacks/ContentView.swift
+++ LA Hacks/ContentView.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var mem: Int = 0;
+
     var body: some View {
+        
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            
+            Button("abc") {
+                mem = os_proc_available_memory();
+            }
+            
+            Text("Memory: \(mem)")
         }
         .padding()
     }
