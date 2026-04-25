295e69a375d69e9fba004d3c8b59db7a8d260aee
Snowplou <Snowplou01@gmail.com>
2026-04-24 22:35:36 -0700

    Initial Commit

---
+++ LA Hacks/LA_HacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LA_HacksApp.swift
+//  LA Hacks
+//
+//  Created by Jacob Scheff on 4/24/26.
+//
+
+import SwiftUI
+
+@main
+struct LA_HacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
