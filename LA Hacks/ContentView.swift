a9dad4c957e275948f60b50615f2ab5ec428df7b
Jacob Scheff <60328567+JacobScheff@users.noreply.github.com>
2026-04-25 23:14:22 -0700

    App Icon, is connected to network (#6)
    * App Icon, is connected to network
    
    * fixed wifi network monitoring causes black bar on top, removing .ignoreSafeArea for top of screen

--- LA Hacks/ContentView.swift
+++ LA Hacks/ContentView.swift
@@ -9,14 +9,37 @@ import Foundation
 import SwiftUI
 import ZeticMLange
 import AVFoundation
+import Network
 
 struct ContentView: View {
     @AppStorage("onboarded")
     private var onboarded: Bool = false
+    
+    @AppStorage("isConnected")
+    private var isConnected: Bool = false
+    
+    // Monitor if connected to wifi
+    let monitor = NWPathMonitor()
+    let queue = DispatchQueue(label: "NetworkMonitor")
+    
+    @State var startedMonitor = false
 
     var body: some View {
         if !onboarded {
             Onboard()
+                .onAppear {
+                    if (!startedMonitor) {
+                        monitor.pathUpdateHandler = { path in
+                            if path.status == .satisfied {
+                                isConnected = true
+                            } else {
+                                isConnected = false
+                            }
+                        }
+                        monitor.start(queue: queue)
+                        startedMonitor = true
+                    }
+                }
         } else {
             LearningGalaxyView()
                 .onAppear {
@@ -25,6 +48,18 @@ struct ContentView: View {
                         let session = AVAudioSession.sharedInstance()
                         try session.setCategory(.playback, mode: .default, options: [])
                         try session.setActive(true)
+                        
+                        if (!startedMonitor) {
+                            monitor.pathUpdateHandler = { path in
+                                if path.status == .satisfied {
+                                    isConnected = true
+                                } else {
+                                    isConnected = false
+                                }
+                            }
+                            monitor.start(queue: queue)
+                            startedMonitor = true
+                        }
                     } catch {
                         print("Failed to set audio session category: \(error)")
                     }
