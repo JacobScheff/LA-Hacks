ec8c353919df7c6b7405b9b0b16e14ae7f035946
Snowplou <Snowplou01@gmail.com>
2026-04-25 21:02:04 -0700

    App Icon, is connected to network

--- LA Hacks/ContentView.swift
+++ LA Hacks/ContentView.swift
@@ -9,12 +9,34 @@ import Foundation
 import SwiftUI
 import ZeticMLange
 import AVFoundation
+import Network
 
 struct ContentView: View {
     @AppStorage("onboarded")
     private var onboarded: Bool = false
+    
+    @AppStorage("isConnected")
+    private var isConnected: Bool = false
+    
+    // Monitor if connected to wifi
+    let monitor = NWPathMonitor()
+    let queue = DispatchQueue(label: "NetworkMonitor")
 
     var body: some View {
+        // Empty element to run code from
+        Color.clear
+            .frame(width: 0, height: 0)
+            .onAppear {
+                monitor.pathUpdateHandler = { path in
+                    if path.status == .satisfied {
+                        isConnected = true
+                    } else {
+                        isConnected = false
+                    }
+                }
+                monitor.start(queue: queue)
+            }
+                
         if !onboarded {
             Onboard()
         } else {
